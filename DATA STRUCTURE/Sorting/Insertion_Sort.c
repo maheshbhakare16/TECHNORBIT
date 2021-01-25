dfe179f77b82f9781558ea5293af8b3ffd4779bd
maheshbhakare16 <maheshbhakare95@gmail.com>
2021-01-25 21:24:31 +0530

    Insertion Sort added

---
+++ DATA STRUCTURE/Sorting/Insertion_Sort.c
@@ -0,0 +1,59 @@
+
+/*
+Title- Insertion Sort
+Author- Bhakare Mahesh Santosh
+ID- 492
+Batch- TechnOrbit(PPA-8)
+*/ 
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void display_list(int* arr, int n)
+{ 
+    int i;
+    printf("[");
+    for(i=0;i<n;i++)
+    {
+        printf("%d , ",*(arr+i));
+    }
+    printf("]\n");
+}
+
+void Insertion_Sort(int* arr , int n)
+{
+    int temp, hole, i;
+    for(i=1;i<n;i++)
+    {
+        temp = *(arr+i);
+        hole = i;
+        printf("***************************************************************************\n");
+        printf("Iterration No. %d\n",i);
+        printf("Sorting element is %d\n",temp);
+        while(hole > 0 && *(arr+(hole-1)) > temp )
+        {
+            *(arr+hole) = *(arr+(hole-1));
+            hole--;
+        }
+        *(arr+hole) = temp;
+        printf("Sorted elemet Changed Position : ");
+        display_list(arr , n);
+    }
+    printf("***************************************************************************\n");
+}
+void main()
+{
+    int *arr , i , n;
+    printf("Enter how many elements do you want to enter: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    printf("Unsorted List is: ");
+    display_list(arr, n);
+    Insertion_Sort(arr, n);
+    printf("Sorted List is: ");
+    display_list(arr, n);
+}
