09bdfc7e96b444af96692ca8decd79199c34aa60
maheshbhakare16 <maheshbhakare95@gmail.com>
2021-01-28 12:47:03 +0530

    quick sort

---
+++ DATA STRUCTURE/Sorting/Quick_Sort.c
@@ -0,0 +1,72 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void display_list(int* arr, int n)
+{ 
+    int i;
+    printf("[");
+    for(i=0;i<n;i++)
+    {
+        printf("%d , ",*(arr+i));
+    }
+    printf("]\n");
+}
+
+void Quick_Sort(int* arr, int L, int H, int n)
+{
+    
+    int low,high,pivot,temp;
+    pivot = *(arr+L);
+    low = L+1;
+    high = H;
+    while(low <= high)
+    {
+        while(*(arr+low) < pivot)
+        {
+            low++;
+        }
+        while(*(arr+high) > pivot)
+        {
+            high--;
+        }
+        
+        if(low <= high)
+        {
+            temp = *(arr+low);
+            *(arr+low) = *(arr+high);
+            *(arr+high) = temp;
+            low++;
+            high--;
+        }
+    }
+    temp = *(arr+L);
+    *(arr+L) = *(arr+high);
+    *(arr+high) = temp;
+    if(L < high)
+    {
+        Quick_Sort(arr, L, high-1,n);
+    }
+    if(H > low)
+    {
+        Quick_Sort(arr,high+1,H,n);
+    }
+}
+void main()
+{
+    int* arr , i, n;
+    printf("Enter how many elements do you want to enter: ");
+    scanf("%d",&n);
+    arr =(int*)malloc(sizeof(int)*n);
+    printf("Enter array Elements: \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    printf("Unsorted List is: ");
+    display_list(arr,n);
+    Quick_Sort(arr,0,n-1,n);
+    printf("Sorted List is: ");
+    display_list(arr,n);
+}
